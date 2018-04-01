2606ca708d284b03a00b63528c7d07ca242ef735
Sachan <asachan@r3aig.com>
2018-04-01 12:11:10 +0100

    addedd code

--- Project.py
+++ Project.py
@@ -1,21 +1,24 @@
 
 #adarsha sachan
 #Project 2018 for Programming and Scripting for Fisher’s Iris data set
+# Base on sepaland petal length ,width i want to match the record
 import csv
-Length = input('Enter Length to find')
-Width = input('Enter Width to find')
-#def findflowername (number,number2):
+SepalLength = input('Enter SepalLength to find')
+SepalWidth = input('Enter SepalWidth to find')
+PetalLength = input('Enter PetalLength to find')
+PetalWidth = input('Enter PetalWidth to find')
+#def findflowername (SepalLength,SepalWidth,PetalLength,PetalWidth):
  # as line by line 
 with open("Data/irish.csv","r") as f:
     reader = csv.reader(f)
     record=""
     for line in reader:
     #for row in line:
-      if (line[0] == Length and line[1] == Width):
+      if (line[0] == SepalLength and line[1] == SepalWidth):
         #print(line)
         record=line[4]
         print (record)
-      elif (line[2] == Length and line[3] == Width):
+      elif (line[2] == PetalLength and line[3] == PetalWidth):
           record=line[4]
           print (record)
       elif record=="":
