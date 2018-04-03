a33a5f5be6b992755002a75e0f1c2665755fbeaa
Sachan <asachan@r3aig.com>
2018-04-03 12:14:39 +0100

    grapth change

--- myproject.py
+++ myproject.py
@@ -1,7 +1,8 @@
 # adarsha sachan
 # i have build my algorithm that will tell whatsa flower name
 # and its without traing set
-
+import pandas as pd
+import matplotlib.pyplot as graph
 while True:
   try:
     sepallength=float( input ("Please Enter the sepallength "))
@@ -52,10 +53,10 @@ sepalarea=(sepallength*sepalWidth)
 #sepalarea=format(sepalarea,'.2f')
 totalarea=(sepallength*sepalWidth)*(Petallength*PetallWidth)
 #totalarea=format(totalarea,'.2f')
-#print(petalarea,sepalarea,totalarea)
+print(petalarea,sepalarea,totalarea)
 #if petal area less then 2 and total area less than 16 its Setosa
  #condition 1
-if petalarea < 2.00 and totalarea < 16.00:
+if petalarea < 2.00 and totalarea < 17.00:
   print("this is Iris-Setos ")
   #condition 2 
 elif (totalarea > 172.00) and (petalarea > 10.10) :
@@ -73,3 +74,8 @@ elif (totalarea < 172.00) and (petalarea > 7.60) and (sepalarea > 16.00 and sepa
 else:
   print("this is ris- virginica ")
 
+creat_coloum =['sepallength' , 'sepalWidth' , 'prtallength' , 'prtalwidth' , 'Flowername' ]
+read=pd.read_table('Data/irish.csv',sep=',',header=None,names=creat_coloum)
+read=read.groupby('Flowername').mean()
+read=read.groupby('Flowername').mean().plot.bar()
+graph.show()
