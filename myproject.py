90abc812fa5e6fb67af1cdbf2f037e9df9758d96
Sachan <asachan@r3aig.com>
2018-04-09 10:59:11 +0100

    cleanup

--- myproject.py
+++ myproject.py
@@ -54,7 +54,7 @@ sepalarea=(sepallength*sepalWidth)
 Perimeter=(sepallength+sepalWidth)+(Petallength+PetallWidth)
 Perimeter=2*Perimeter
 #totalarea=format(totalarea,'.2f')
-print(petalarea,Perimeter)
+#print(petalarea,Perimeter)
 #if petal area less then 2 and total area less than 16 its Setosa
  #condition 1
 if petalarea < 2.00 and Perimeter < 25.00:
@@ -62,7 +62,7 @@ if petalarea < 2.00 and Perimeter < 25.00:
   Flo="Iris-setosa"
   print("Based on my analysis this is",Flo)
   #condition 2 
-elif (Perimeter < 33.00) and (petalarea < 7.00) :
+elif (Perimeter < 33.00) and (petalarea < 9.00) :
   #print("this is ris- virginica ")
   Flo="Iris-versicolor"
   print("Based on my analysis this is",Flo)
@@ -71,27 +71,33 @@ else:
   Flo="Iris-virginica"
   print("Based on my analysis this is",Flo)
 # creat colum for each row ,
-creat_coloum =['sepallength' , 'sepalWidth' , 'prtallength' , 'prtalwidth' , 'Flowername','PetalArea', 'PerimeterArea']
+creat_coloum =['sepallength' , 'sepalWidth' , 'petallength' , 'petalwidth' , 'Flowername','PetalArea', 'PerimeterArea']#,'SepalArea']
 # adding header and cloum
 read=pd.read_table('Data/irish.csv',sep=',',header=None,names=creat_coloum)
+#print(read)
 readmean=read.groupby('Flowername').mean()
-read['PetalArea']=read['prtallength']*read['prtalwidth']
-read['PerimeterArea']=2*(read['prtallength']+read['prtalwidth']+read['sepallength']+read['sepalWidth'])
+#print(readmean)
+# create entry for sepal ,petal,perimeter area
+read['PetalArea']=read['petallength']*read['petalwidth']
+#read['SepalArea']=read['sepallength']*read['sepalWidth']
+read['PerimeterArea']=2*(read['petallength']+read['petalwidth']+read['sepallength']+read['sepalWidth'])
 #print(read['PetalArea'])
-print(read['PerimeterArea'])
+#print(read['PerimeterArea'])
 # creating mean bar pannel
-test=read.groupby('Flowername').mean().plot(kind='bar')
+BAR=read.groupby('Flowername').mean().plot(kind='bar')
+mean=BAR=read.groupby('Flowername').mean()
+#print(mean)
 graph.show()
-# reading each flower index
+# reading each flower record index
 Flower=read['Flowername']
 sepalX=read['sepallength']
 sepalY=read['sepalWidth']
-PetalX=read['prtallength']
-PetalY=read['prtalwidth']
-# base on flower name for Sepal providing graph as well my record but thay are diff mark
+PetalX=read['petallength']
+PetalY=read['petalwidth']
+# based on flower name [X cordinate] for Sepal length and width creating scatter graph
 X=graph.scatter(Flower,sepalX,color=['b'])
 y=graph.scatter(Flower,sepalY,color=['c'])
-# my input record sepal
+# Representing my input record in sepal VS identified flower
 X=graph.scatter(Flo,sepallength,color=['r'],s=34,marker="*")
 y=graph.scatter(Flo,sepalWidth,color=['m'],s=34,marker="*")
 # x and y cordinate added lable
@@ -102,10 +108,10 @@ graph.title('For Sepal record in Scatter ')
 # in grf show legend
 graph.legend()
 graph.show()
-# base on flower name for Petal providing graph as well my record but thay are diff mark
+## based on flower name [X cordinate] for Petal length and width creating scatter graph
 t=graph.scatter(Flower,PetalX,color=['r'])
 s=graph.scatter(Flower,PetalY,color=['m'])
-# my input record for petal
+# Representing my input record in Petal VS identified flower
 X=graph.scatter(Flo,Petallength,color=['b'],s=34,marker="*")
 y=graph.scatter(Flo,PetallWidth,color=['c'],s=34,marker="*")
 graph.xlabel('Xcoordinate OF FLOWER')
@@ -129,9 +135,10 @@ graph.ylabel('Ycoordinate OF FLOWER')
 graph.title('For ALL record in Scatter ')
 graph.legend()
 graph.show()
-# my logic graph Scatter
+# my logic graph in Scatter form
 t=graph.scatter(Flower,read['PerimeterArea'])
 t=graph.scatter(Flower,read['PetalArea'])
+#t=graph.scatter(Flower,read['SepalArea'])
 graph.xlabel('Xcoordinate OF FLOWER')
 graph.ylabel('Ycoordinate OF FLOWER')
 graph.title('For based on My logic  Scatter ')
