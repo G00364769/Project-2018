3b4588a6e02702a450acfc8068afa6c1a1198a31
Sachan <asachan@r3aig.com>
2018-04-21 23:45:32 +0100

    sklearn code

--- Project.py
+++ Project.py
@@ -1,78 +1,82 @@
 
 #adarsha sachan
 #Project 2018 for Programming and Scripting for Fisher’s Iris data set
-# Base on sepaland petal length ,width i want to match the record
-import csv
-SepalLength = input('Enter SepalLength to find')
-SepalWidth = input('Enter SepalWidth to find')
-PetalLength = input('Enter PetalLength to find')
-PetalWidth = input('Enter PetalWidth to find')
-#def findflowername (SepalLength,SepalWidth,PetalLength,PetalWidth):
- # as line by line 
- with open("Data/irish.csv","r") as f:
-    reader = csv.reader(f)
-    record=""
-    for line in reader:
-    #for row in line:
-      if (line[0] == SepalLength and line[1] == SepalWidth):
-        #print(line)
-        record=line[4]
-        print (record)
-      elif (line[2] == PetalLength and line[3] == PetalWidth):
-          record=line[4]
-          print (record)
-      elif record=="":
-         record="there is NoMatch Found"
-         print (record)
-
-# using Sklern liberary code
-from sklearn.datasets import load_iris
-from sklearn import tree
-from sklearn.metrics import accuracy_score
-import numpy as np
 #http://docs.python-guide.org/en/latest/scenarios/ml/
 #http://scikit-learn.org/stable/user_guide.html
 #https://www.python-course.eu/machine_learning_with_scikit.php
 #http://scikit-learn.org/stable/tutorial/basic/tutorial.html
-
+#http://www.pythonforbeginners.com/os/pythons-os-module
+import os
+#https://docs.python.org/3/library/pickle.html
+import pickle
+#http://scikit-learn.org/stable/tutorial/basic/tutorial.html#machine-learning-the-problem-setting
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import numpy as np
+#http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeRegressor.html
+from sklearn.tree import DecisionTreeRegressor
+sepallength=float( input ("Please Enter the sepallength "))
+sepalWidth=float( input ("Please Enter the sepallength "))
+Petallength=float( input ("Please Enter the sepallength "))
+PetallWidth=float( input ("Please Enter the sepallength "))
 #loading the iris dataset
 iris = load_iris()
-
 x = iris.data #array of the data
-#print(x)
-y = iris.target #array of labels of each data entry
-#print(y)
-
-#getting label names i.e the three flower species
+#print("Full Record in array",x)
+y = iris.target #array of labels of each data entry as flower name
+#print("labels of each data entry",y)
+#getting label names the three flower species
 y_names = iris.target_names
-#print(y_names)
-
+#print("Flower name",y_names)
 #taking random indices to split the dataset into train and test
 test_ids = np.random.permutation(len(x))
 #test_ids=np.permutation(len(x))
 #print(test_ids)
-
 #splitting data and labels into train and test
 #keeping last 10 entries for testing, rest for training
-
-x_train = x[test_ids[:-10]]
-#print(x_train)
-x_test = x[test_ids[-10:]]
-#print(x_test)
-
-y_train = y[test_ids[:-10]]
-#print(y_train)
-y_test = y[test_ids[-10:]]
+TrainData = x[test_ids[:-60]]
+#print(TrainData)
+#InputTrainData = x[test_ids[-10:]]
+#print(InputTrainData)
+TrainFlower = y[test_ids[:-60]]
+#print(TrainFlower)
+y_test = y[test_ids[-60:]]
 #print(y_test)
-
+#print(y_test)
 #classifying using decision tree
 clf = tree.DecisionTreeClassifier()
-
 #training the classifier with the training set
-clf.fit(x_train, y_train)
-
+clf.fit(TrainData, TrainFlower)
+InputTrainData = x[test_ids[-60:]]
+#InputTrainData = [[sepallength,sepalWidth,Petallength,PetallWidth]]
 #predictions for test dataset
-pred = clf.predict(x_test)
-print (pred )#predicted labels i.e flower species
-print (y_test) #actual labels
-print (float(accuracy_score(pred, y_test))*100) #prediction accuracy
+pred = clf.predict(InputTrainData)
+#print ("pred", pred )#predicted labels i.e flower species
+accuracy= (float(accuracy_score(pred, y_test))*100) #prediction accuracy
+print("prediction percentage",accuracy)
+# using DecisionTreeRegressor mim and max value
+tree = DecisionTreeRegressor(max_depth=10, random_state=0).fit(TrainData , TrainFlower)
+model_dir = "\model"
+# this will creat path in destop
+Path = os.path.dirname(os.getcwd())
+print (Path)
+#SAV is a file extension used for the saved date of SPSS (Statistical Package for the Social Sciences).
+filename = Path + '\\finalized_model.sav'
+print("Creating model path in destop",filename)
+pickle.dump(tree, open(filename, 'wb'))
+filename =  Path + '\\finalized_model.sav'
+# providing user input record in model
+print("Checking input record In build in model") 
+loaded_model = pickle.load(open(filename, 'rb'))
+#predicted = loaded_model.predict([a,b,c,d].reshape(1, -1))
+predicted= loaded_model.predict([[sepallength,sepalWidth,Petallength,PetallWidth]])
+print("predicted value form model set",predicted)
+if predicted == 0:
+  print("prediction percentage is",accuracy,"Iris-setosa")
+elif predicted == 1:
+  print("prediction percentage is",accuracy,"Iris-versicolor")
+elif predicted ==2:
+  print("prediction percentage is",accuracy,"Iris-virginica")
+
+  
--- myproject.py
+++ myproject.py
@@ -1,5 +1,5 @@
 # adarsha sachan
-# i have build my algorithm that will tell whatsa flower name
+# i have build my own algorithm that will tell me flower name
 # and its with dataset set analysis
 import pandas as pd
 import matplotlib.pyplot as graph
@@ -154,3 +154,78 @@ graph.ylabel('Ycoordinate of PerimeterArea and PetalArea')
 graph.title('Scatter graph based on PerimeterArea and PetalArea ')
 graph.legend()
 graph.show()
+
+print("Second method using sklearn running program   ")
+#http://docs.python-guide.org/en/latest/scenarios/ml/
+#http://scikit-learn.org/stable/user_guide.html
+#https://www.python-course.eu/machine_learning_with_scikit.php
+#http://scikit-learn.org/stable/tutorial/basic/tutorial.html
+#http://www.pythonforbeginners.com/os/pythons-os-module
+import os
+#https://docs.python.org/3/library/pickle.html
+import pickle
+#http://scikit-learn.org/stable/tutorial/basic/tutorial.html#machine-learning-the-problem-setting
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import numpy as np
+#http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeRegressor.html
+from sklearn.tree import DecisionTreeRegressor
+#loading the iris dataset
+iris = load_iris()
+x = iris.data #array of the data
+#print("Full Record in array",x)
+y = iris.target #array of labels of each data entry as flower name
+#print("labels of each data entry",y)
+#getting label names the three flower species
+y_names = iris.target_names
+#print("Flower name",y_names)
+#taking random indices to split the dataset into train and test
+test_ids = np.random.permutation(len(x))
+#test_ids=np.permutation(len(x))
+#print(test_ids)
+#splitting data and labels into train and test
+#keeping last 10 entries for testing, rest for training
+TrainData = x[test_ids[:-60]]
+#print(TrainData)
+#InputTrainData = x[test_ids[-10:]]
+#print(InputTrainData)
+TrainFlower = y[test_ids[:-60]]
+#print(TrainFlower)
+y_test = y[test_ids[-60:]]
+#print(y_test)
+#print(y_test)
+#classifying using decision tree
+clf = tree.DecisionTreeClassifier()
+#training the classifier with the training set
+clf.fit(TrainData, TrainFlower)
+InputTrainData = x[test_ids[-60:]]
+#InputTrainData = [[sepallength,sepalWidth,Petallength,PetallWidth]]
+#predictions for test dataset
+pred = clf.predict(InputTrainData)
+#print ("pred", pred )#predicted labels i.e flower species
+accuracy= (float(accuracy_score(pred, y_test))*100) #prediction accuracy
+print("prediction percentage",accuracy)
+# using DecisionTreeRegressor mim and max value
+tree = DecisionTreeRegressor(max_depth=10, random_state=0).fit(TrainData , TrainFlower)
+model_dir = "\model"
+# this will creat path in destop
+Path = os.path.dirname(os.getcwd())
+#print (Path)
+#SAV is a file extension used for the saved date of SPSS (Statistical Package for the Social Sciences).
+filename = Path + '\\finalized_model.sav'
+print("Creating model path in destop",filename)
+pickle.dump(tree, open(filename, 'wb'))
+filename =  Path + '\\finalized_model.sav'
+# providing user input record in model
+print("Checking input record In build in model") 
+loaded_model = pickle.load(open(filename, 'rb'))
+#predicted = loaded_model.predict([a,b,c,d].reshape(1, -1))
+predicted= loaded_model.predict([[sepallength,sepalWidth,Petallength,PetallWidth]])
+print("predicted value form model set",predicted)
+if predicted == 0:
+  print("prediction percentage is",accuracy,"Iris-setosa")
+elif predicted == 1:
+  print("prediction percentage is",accuracy,"Iris-versicolor")
+elif predicted ==2:
+  print("prediction percentage is",accuracy,"Iris-virginica")
